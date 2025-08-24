56bab41330a027b9894a8f7b42c0c9e160f18cf1
dostonsulaymon <dostonqosimiy19@gmail.com>
2025-08-24 17:32:50 +0500

    some updates

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Backend Service Template
+ # Backend Service Template
 
 ---
 
--- lint-staged.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  '**/*.(ts|js)?(x)': (filenames) => [
-    `eslint --fix ${filenames.map((file) => `"${file}"`).join(' ')}`,
-    `prettier --write ${filenames.map((file) => `"${file}"`).join(' ')}`,
-  ],
-};
